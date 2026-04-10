1bb3a3179e002ef268498d8cb24571b7dc22f3f2
Marcos <maik.cok14@gmail.com>
2026-04-10 02:07:13 -0300

    fix: disable ssr and prerender for netlify build

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Deixe vazio por enquanto, mas garanta que o arquivo foi salvo
+];
