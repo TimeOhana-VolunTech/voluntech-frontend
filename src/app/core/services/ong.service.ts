ab9db2071d38c899d9e3333cfae856c7d258e614
Marcos <maik.cok14@gmail.com>
2026-04-16 01:35:17 -0300

    fix: configura environments para deploy no Netlify

--- src/app/core/services/ong.service.ts
+++ src/app/core/services/ong.service.ts
@@ -1,13 +1,12 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Ong, OngRequest } from '../models/ong.model';
-//import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class OngService {
   private http = inject(HttpClient);
-  private readonly API = 'http://localhost:8080/ongs';
-  //private readonly API = `${environment.apiUrl}/ongs`;
+  private readonly API = `${environment.apiUrl}/ongs`;
 
   cadastrar(ong: OngRequest) {
     return this.http.post<Ong>(this.API, ong);
